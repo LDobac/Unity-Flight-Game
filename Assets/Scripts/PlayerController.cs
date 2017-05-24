9c7b1f05044b30e27d2ac76fe4e8262350e2f095
LDobac <ljh34210329@gmail.com>
2017-05-24 21:04:52 +0900

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+
+	private void Start()
+	{
+
+	}
+	private void Update()
+	{
+		FlightMove();
+	}
+
+	private void FlightMove()
+	{
+		float verControl = Input.GetAxis("Vertical");
+		float horControl = Input.GetAxis("Horizontal");
+
+		Vector3 move = transform.position + new Vector3(horControlverControl,,0.0f) * speed;
+		transform.position = move;
+	}
+}
